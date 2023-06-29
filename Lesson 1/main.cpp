4ebc1d15cade55f077dbd2fbeafdf965d2cbce6d
Osteopourosis <adamtantalum@gmail.com>
2023-06-29 21:51:42 +0100

    changes to main

--- Lesson 1/main.cpp
+++ Lesson 1/main.cpp
@@ -9,13 +9,30 @@ using namespace std;
 //3. Protected - can be accessed by members and friends of the class, but not from derived classes
 // derived classes are classes that inherit from a base class (parent class) 
 class Employee{
+public:
     string Name;
     string Company;
     int Age;
+
+
+    void IntroduceYourself(){
+        cout << "Name - " << Name << endl;
+        cout << "Company - " << Company << endl;
+        cout << "Age - " << Age << endl;
+    }
 };
 
 int main()
 {
     Employee employee1;
-    employee1.Name = "Saldina";   
+    employee1.Name = "AE";   
+    employee1.Company = "Icl";
+    employee1.Age = 18;
+    employee1.IntroduceYourself();
+
+    Employee employee2;
+    employee2.Name = "John";
+    employee2.Company = "Amazon";
+    employee2.Age = 35;
+    employee2.IntroduceYourself();
 }
