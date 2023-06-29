2d744ddc3a2388a4208802888793aeeaed59dbc5
Osteopourosis <adamtantalum@gmail.com>
2023-06-29 21:41:37 +0100

    modified changes

--- Lesson 1/main.cpp
+++ Lesson 1/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+using namespace std;
+#include <string>
 
-int main(){
+//3 types of access modifiers in C++ : 
+//1. Public - can be accessed anywhere
+//2. Private - can only be accessed by members or friends of the class
+// friends of a class are functions or other classes that are granted access to private and protected members of the class
+//3. Protected - can be accessed by members and friends of the class, but not from derived classes
+// derived classes are classes that inherit from a base class (parent class) 
+class Employee{
+    string Name;
+    string Company;
+    int Age;
+};
 
-
-    
+int main()
+{
+    Employee employee1;
+    employee1.Name = "Saldina";   
 }
