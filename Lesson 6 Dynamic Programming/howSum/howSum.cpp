ea68ab43f317a42c18fbbf2521615abe36665ff2
Osteopourosis <adamtantalum@gmail.com>
2023-07-22 16:05:04 +0100

    Updated files

--- Lesson 6 Dynamic Programming/howSum/howSum.cpp
+++ Lesson 6 Dynamic Programming/howSum/howSum.cpp
@@ -2,32 +2,37 @@
 #include <map>
 #include <vector>
 
-std::vector<int> howSum(int target, std::vector<int>& arr, std::map<int, std::vector<int>>& memo) {
-    if (memo.count(target) > 0) {
+std::vector<int> howSum(int target, std::vector<int> arr, std::map<int, std::vector<int>>& memo) {
+    if (memo.find(target) != memo.end()) {
         return memo[target];
     }
     if (target == 0) {
         return {};
     }
     if (target < 0) {
-        return {};
+        memo[target] = {}; // Use an empty vector for invalid combinations
+        return {-1};
     }
-    for (int x : arr) {
-        int remainder = target - x;
-        memo[target] = howSum(remainder, arr, memo);
-        memo[target].push_back(x);
-        if (!memo[target].empty()) {
-            return memo[target];
+
+    for (int i = 0; i < arr.size(); i++) {
+        int remainder = target - arr[i];
+        auto remainderResult = howSum(remainder, arr, memo);
+        if (remainderResult[0] != -1) {
+            std::vector<int> updatedResult = remainderResult;
+            updatedResult.push_back(arr[i]);
+            memo[target] = updatedResult;
+            return updatedResult;
         }
     }
-    memo[target] = {};
-    return {};
+
+    memo[target] = {}; // Use an empty vector for invalid combinations
+    return {-1};
 }
 
 int main() {
     std::map<int, std::vector<int>> memo;
     std::vector<int> arr = {6, 34, 45};
-    std::vector<int> result = howSum(10, arr, memo);
+    std::vector<int> result = howSum(40, arr, memo);
     for (int num : result) {
         std::cout << num << " ";
     }
